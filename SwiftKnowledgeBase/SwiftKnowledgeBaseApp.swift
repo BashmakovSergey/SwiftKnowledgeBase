92ec31363c0b08050a6e8f48724ddc636c062eaa
BashmakovSergey <bashmakovsergey@yandex.ru>
2024-02-07 00:22:31 +0300

    Initial Commit

---
+++ SwiftKnowledgeBase/SwiftKnowledgeBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftKnowledgeBaseApp.swift
+//  SwiftKnowledgeBase
+//
+//  Created by Сергей Башмаков on 07.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftKnowledgeBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
